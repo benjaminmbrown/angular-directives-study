a0c5e750b093351510231dababcf1cc3a10f4ce4
Benjamin M. Brown <benjaminbb@gmail.com>
2015-09-26 16:38:52 -0500

    Adding table helper directive that utilizes ng-model instead of custom elements

--- link-function-study/directivesController.js
+++ link-function-study/directivesController.js
@@ -85,6 +85,167 @@
 
     })
 
+app.directive('tableHelperWithNgModel', function(){
+
+        return{
+            template: '<div class="tableHelper"></div>',
+            retrict: 'E',
+            //?ngModel make it optional
+            //^ngModel make it on the element
+            //^^ngModel make it on parent level
+            //?^ngModel make it optional, on the element
+            require: 'ngModel',
+            scope: {
+                columnmap: '=',
+            },
+            link: function(scope,element,attr, ngModel){
+            var headerCols = [],
+                tableStart = '<table>',
+                tableEnd = '</table>',
+                table = '',
+                visibleProps = [],
+                datasource,
+                sortCol = null,
+                sortDir = 1;
+
+              //  scope.$watchCollection('datasource', render);
+
+                //watch for ngModel to change. observes attribute ngModel and watches it's value
+               
+
+               //Technique 1 - watch for normal attributes
+                // attr.$observe('ngModel', function(value){
+                //     scope.$watch(value, function(newValue){
+                //         render();
+                //     })
+                // });
+
+                //technique 2
+               // scope.$watch(attrs.ngModel, render);
+
+               //Technique 3
+               // scope.$watch(function(){
+               //  return ngModel.$modelValue;
+               // }, function(newValue){
+               //  render();
+               // })
+
+               //Technique 4 - best when doing ngModel
+               ngModel.$render = function(){
+                console.log(ngModel.$modelValue)
+                render();
+               }
+
+                wireEvents();
+
+                function render() {
+                 if(ngModel && ngModel.$modelValue.length){
+                     scope.datasource = ngModel.$modelValue;
+                     console.log('datsource', datasource);
+                      table += tableStart;
+                      table += renderHeader();
+                      table += renderRows() + tableEnd;
+                      renderTable();
+                  }
+              }
+
+              function wireEvents() {
+                  element.on('click', function(event) {
+                   if (event.srcElement.nodeName === 'TH') {
+                       var val = event.srcElement.innerHTML;
+                       var col = (scope.columnmap) ? getRawColumnName(val) : val;
+                       if (col) sort(col);
+                   }
+               });
+              }
+
+              function sort(col) {
+              //See if they clicked on the same header
+              //If they did then reverse the sort
+              if (sortCol === col) sortDir = sortDir * -1;
+              sortCol = col;
+              scope.datasource.sort(function(a,b) {
+               if (a[col] > b[col]) return 1 * sortDir;
+               if (a[col] < b[col]) return -1 * sortDir;
+               return 0;
+           });
+              render();
+          }
+
+          function renderHeader() {
+             var tr = '<tr>';
+         
+             for (var prop in scope.datasource[0]) {
+                 var val = getColumnName(prop);
+                 if (val) {
+                       //Track visible properties to make it fast to check them later
+                       visibleProps.push(prop);
+                       tr += '<th>' + val + '</th>';
+                   }
+               }
+               tr += '</tr>';
+               tr = '<thead>' + tr + '</thead>';
+               return tr;
+           }
+
+           function renderRows() {
+             var rows = '';
+             for (var i = 0, len = scope.datasource.length; i < len; i++) {
+                rows += '<tr>';
+                var row = scope.datasource[i];
+                for (var prop in row) {
+                    if (visibleProps.indexOf(prop) > -1) {
+                        rows += '<td>' + row[prop] + '</td>';
+                    }
+                }
+                rows += '</tr>';
+            }
+            rows = '<tbody>' + rows + '</tbody>';
+            return rows;
+        }
+
+        function renderTable() {
+          table += '<br /><div class="rowCount">' + scope.datasource.length + ' rows</div>';
+          element.html(table);
+          table = '';
+      }
+
+      function getRawColumnName(friendlyCol) {
+          var rawCol;
+          scope.columnmap.forEach(function(colMap) {
+              for (var prop in colMap) {
+                  if (colMap[prop] === friendlyCol) {
+                   rawCol = prop;
+                   break;
+               }
+           }
+           return null;
+       });
+          return rawCol;
+      }
+
+      function filterColumnMap(prop) {
+          var val = scope.columnmap.filter(function(map) {
+              if (map[prop]) {
+                  return true;
+              }
+              return false;
+          });
+          return val;
+      }
+
+      function getColumnName(prop) {
+          if (!scope.columnmap) return prop;
+          var val = filterColumnMap(prop);
+          if (val && val.length && !val[0].hidden) return val[0][prop];
+          else return null;
+      }
+
+            }
+        }
+
+    })
+
     app.directive('tableHelper', function(){
 
         return{
