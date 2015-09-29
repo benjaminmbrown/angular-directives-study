d233fb59ad857b135748e5ff5c2b76137e3c8867
Benjamin M. Brown <benjaminbb@gmail.com>
2015-09-28 23:06:36 -0500

    Using a controller + template instead of DOM manipulation in link function

---
+++ controllers-in-directives/controllersDirectiveModule.js
@@ -0,0 +1,102 @@
+(function() {
+
+    var app = angular.module('controllersDirectiveModule', []);
+
+    var injectParams = ['$scope'];
+
+    var CustomersController = function ($scope) {
+        var counter = 0;
+        $scope.tasks = [{ title: 'Task 1' }];
+        $scope.customer = {
+            name: 'David',
+            street: '1234 Anywhere St.'
+        };
+
+        $scope.customers = [
+        {
+            name: 'David',
+            street: '1234 Anywhere St.',
+            age: 25,
+            url: 'index.html'
+        },
+        {
+            name: 'Tina',
+            street: '1800 Crest St.',
+            age: 35,
+            url: 'index.html'
+        },
+        {
+            name: 'Michelle',
+            street: '890 Main St.',
+            age: 29,
+            url: 'index.html'
+        },
+        {
+            name: 'John',
+            street: '444 Cedar St.',
+            age: 18,
+            url: 'index.html'
+        }
+        ];
+
+        $scope.addCustomer = function (name) {
+            console.log(name);
+            counter++;
+            $scope.customers.push({
+                name: (name) ? name : 'New Customer' + counter,
+                street: counter + ' Cedar Point St.',
+                age: counter
+            });
+        };
+
+        $scope.changeData = function () {
+            counter++;
+            $scope.customer = {
+                name: 'James',
+                street: counter + ' Cedar Point St.'
+            };
+        };
+    };
+
+    CustomersController.$inject = injectParams;
+
+    app.controller('CustomersController', CustomersController);
+
+    app.directive('withController', function(){
+
+        return {
+            restrict: 'EA',
+            scope: { 
+              datasource: '=',
+              add: '&'
+            }, 
+            template: '<button ng-click="addItem()"</button><ul><li ng-repeat="item in items">{{ ::item.name}}</li></ul>',
+            controller: function($scope){
+              init();
+
+              function init(){
+                $scope.items = angular.copy($scope.datasource);
+              }
+
+              $scope.addItem = function(){
+
+                var name = 'New controller item';
+
+                $scope.add()(name);
+
+                $scope.items.push({
+                  name: name
+                });
+              }
+
+            }
+            
+            }
+
+    });
+
+
+
+
+
+}());
