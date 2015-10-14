105bbf4f7a9575585f7403a82b2552497b3b86a2
Benjamin M. Brown <benjaminbb@gmail.com>
2015-10-13 22:03:23 -0500

    WIP saving progress

--- async-overlay-directive/bbOverlayModule.js
+++ async-overlay-directive/bbOverlayModule.js
@@ -2,8 +2,16 @@
 
     var app = angular.module('bbOverlayModule', []);
 
+    app.factory('httpInterceptor',function(){
+        return{};
+    })
+
+    app.config(['$httpProvider', function($httpProvider){
+        $httpProvider.interceptors.push('httpInterceptor');
+    }])
+
     //assuming you have a data service you're passing in
-    app.directive('bbOverlay', ['$http', 'dataService' ,function($http, dataService){
+    app.directive('bbOverlay', ['$q','$timeout','$window', 'httpProvider',function($q, $timeout,$window, httpProvider){
         return {
             restrict: 'EA',
             transclude : true,
@@ -12,7 +20,72 @@
                     '<div id="overlay-background" class="overlayBackground"></div>'+
                     '<div id="overlay-content" class="overlayContent" ng-transclude></div></div>';
             link: function(scope,elem,attrs){
-                var delay = attrs.bbOverlayDelay;
+
+                var overlayContainer = null,
+                timerPromise = null,
+                timerPromiseHide = null,
+                queue = [];
+
+                init();
+
+
+                function init(){
+                 setupHttpInterceptor();
+                 if(window.jQuery) wirejQueryInterceptor();
+                 overlayContainer = elem[0].firstChild;//template location
+                }
+
+                function setupHttpInterceptor(){
+                    httpInterceptor.request = function(config){
+                        processRequest();
+                        return config || $q.when(config);
+                    }
+
+                     httpInterceptor.response = function(response){
+                           processResponse();
+                        return response || $q.when(response);
+                    }
+
+                     httpInterceptor.responseError = function(rejection){
+                        processResponse();
+                        return $q.reject(rejection)
+                    }
+                }
+
+                function wirejQueryInterceptor(){
+                    $(document).ajaxStart(function(){
+                        processRequest();
+                    })
+
+                      $(document).ajaxComplete(function(){
+                        processResponse();
+                    })
+
+                        $(document).ajaxError(function(){
+                        processResponse();
+                    })
+                }
+
+                function processRequest(){
+                    queue.push({});
+                    if(queue.length == 1){
+                        timerPromise = $timeout(function(){
+                            if(queue.length) showOverlay();    
+                        }, 
+                        scope.bbOverlay ? scope.bbOverlayDelay : 500);
+                    }
+
+                }
+
+                function processResponse(){
+                    queue.pop();
+                    if(queue.legth == 0){
+                        
+                    }
+
+                }
+
+
             }
         }
     }]);
