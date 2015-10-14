dc16c4385b41a4b4f703196350c0fbb6b9740748
Benjamin M. Brown <benjaminbb@gmail.com>
2015-10-13 21:27:54 -0500

    Add ng model async validator unique function

--- bb-unique-directive/bbUniqueModule.js
+++ bb-unique-directive/bbUniqueModule.js
@@ -2,22 +2,41 @@
 
     var app = angular.module('bbUniqueModule', []);
 
-
-    app.directive('bbUnique', ['$q',function($q){
-       //allows us to cache template before we run link
-
+    //assuming you have a data service you're passing in
+    app.directive('bbUnique', ['$q', 'dataService' ,function($q, dataService){
         return {
             restrict: 'A',
             require: 'ngModel',
             link: function(scope,elem,attrs,ngModel){
 
-            }
+                ngModel.$asyncValidators.unique = function(modelValue,viewValue){
+                var deffered = $q.defer(),
+                    currentValue = modelValue || viewValue,//if model value is not there, default to view
+                    key = attrs.bbUniqueKey,
+                    property = attrs.bbUniqueProperty;
 
-         
-           
+                    if(key && property){
+                        dataService.checkUniqueValue(key,property,currentValue)
+                            .then(function(unique){
+                                if(unique){
+                                    deferred.resolve(); //got a unique val
+                                }
+                                else{
+                                    deferred.reject();
+                                }
+                            });
+
+                    return deferred.promise;
+
+                    }
+                    else{
+                        return $q.when(true);
+                    }
+
+                }
 
             }
+        }
     }]);
-
 
 }());
